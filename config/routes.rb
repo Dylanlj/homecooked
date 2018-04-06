6a02728dad9eef40c42bcbf6f39ceeb34228f0a1
Piotr <bartnic1@gmail.com>
2018-04-06 15:32:01 +0000

    bootstrap working

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,6 @@ Rails.application.routes.draw do
     resources :meal_ratings, except: [:show, :edit]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    root to: 'postings#index'
 
     get '/login', to: 'posting#index'
 
