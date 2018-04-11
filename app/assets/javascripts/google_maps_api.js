c857418212ffd7e729901f28cc4b3a6fcac837f9
Dylanlj <dylanl-j@hotmail.ca>
2018-04-11 17:09:41 +0000

    added marker css

--- app/assets/javascripts/google_maps_api.js
+++ app/assets/javascripts/google_maps_api.js
@@ -6,7 +6,8 @@ let yourMarker
 let postingMarkers = []
 let labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 let labelIndex = 0
-
+// unformatted addresses
+let userAddresses = []
 
 function codeAddress(address, callback) {
   geocoder.geocode( { 'address': address}, function(results, status) {
@@ -19,6 +20,8 @@ function codeAddress(address, callback) {
 }
 
 function initialize() {
+
+labelIndex = 0
 
   geocoder = new google.maps.Geocoder();
 
@@ -38,65 +41,67 @@ function initialize() {
     });
 
   }
-  const setMapAndYourMarker = new Promise((resolve, reject) => {
-    codeAddress(document.getElementById('googleMap').dataset.userLocation, setUpMap)
-    resolve(mealPostingMarkers)
-  })
-  setMapAndYourMarker.then((callback) => {
-    callback()
-  })
+  codeAddress(document.getElementById('googleMap').dataset.userLocation, setUpMap)
+  mealPostingMarkers()
+}
 
+// this is making an unecessary amout of geocode calls, fix later if your have time
+function orderMarkerByProximity(geoObject) {
+// console.log(postingMarkers)
+  labelIndex = 0
+
+  postingMarkers.sort(function(a, b){
+    let aLat = a.position.lat()
+    let aLng = a.position.lng()
+    let bLat = b.position.lat()
+    let bLng = b.position.lng()
+    let mLat = geoObject[0].geometry.location.lat()
+    let mLng = geoObject[0].geometry.location.lng()
+
+    let aDistance = Math.sqrt(Math.pow((aLat - mLat), 2) + Math.pow((aLng - mLng), 2))
+    let bDistance = Math.sqrt(Math.pow((bLat - mLat), 2) + Math.pow((bLng - mLng), 2))
+    return (aDistance - bDistance)
+  })
+  for (let marker of postingMarkers) {
+    marker.label = labels[labelIndex++ % labels.length]
+    marker.setMap(map)
+  }
 
 }
 
 
-function orderMarkerByProximity(geoObject) {
+function addLabelsToDOM () {
+  $(".circle").
+  console.log(postingMarkers)
+}
 
-  labelIndex = 0
+function mealMarkerCallback (geoObject) {
 
-    postingMarkers.sort(function(a, b){
-      console.log(`inside the sort function${yourMarker}`)
-      let aLat = a.position.lat()
-      let aLng = a.position.lng()
-      let bLat = b.position.lat()
-      let bLng = b.position.lng()
-      let mLat = geoObject[0].geometry.location.lat()
-      let mLng = geoObject[0].geometry.location.lng()
+  let marker = new google.maps.Marker({
+    map: map,
+    position: geoObject[0].geometry.location,
+    originalAddress: userAddresses[labelIndex],
+    label: labels[labelIndex++ % labels.length],
+    formattedAddress: geoObject[0].formatted_address
+  })
+  postingMarkers.push(marker)
 
-      let aDistance = Math.sqrt(Math.pow((aLat - mLat), 2) + Math.pow((aLng - mLng), 2))
-      let bDistance = Math.sqrt(Math.pow((bLat - mLat), 2) + Math.pow((bLng - mLng), 2))
-      return (aDistance - bDistance)
-    })
-  for (let marker of postingMarkers) {
-
-    marker.label = labels[labelIndex++ % labels.length]
-    marker.setMap(map)
-  }
+  codeAddress(document.getElementById('googleMap').dataset.userLocation, orderMarkerByProximity)
 }
 
 function mealPostingMarkers() {
 
-  function mealMarkerCallback (geoObject) {
-
-    let marker = new google.maps.Marker({
-      map: map,
-      position: geoObject[0].geometry.location,
-      label: labels[labelIndex++ % labels.length],
-      address: geoObject[0].formatted_address
-    })
-    postingMarkers.push(marker)
-    codeAddress(document.getElementById('googleMap').dataset.userLocation, orderMarkerByProximity)
-  }
   for (let marker of postingMarkers){
     marker.setMap(null)
   }
   postingMarkers.length = 0
 
-  let userAddresses = []
+
   for (let element of document.getElementsByClassName("meal-div")) {
     let address = element.dataset.address
     if (!userAddresses.includes(address)){
       userAddresses.push(address)
+
       if (userAddresses.length === 10) {
         setTimeout(function() {codeAddress(address, mealMarkerCallback)}, 1300);
       } else {
@@ -105,6 +110,11 @@ function mealPostingMarkers() {
       }
     }
   }
+  // console.log(postingMarkers)
+  //   for (let marker of postingMarkers) {
+  //   console.log(marker.formattedAddress)
+  // }
+  addLabelsToDOM()
 
 }
 
