a7e3e3eae32df308ad8165b444e75f861f233a38
Dylanlj <dylanl-j@hotmail.ca>
2018-04-11 14:37:37 +0000

    fixed signup signup bug and location moving bug

--- app/assets/javascripts/google_maps_api.js
+++ app/assets/javascripts/google_maps_api.js
@@ -19,6 +19,7 @@ function codeAddress(address, callback) {
 }
 
 function initialize() {
+
   geocoder = new google.maps.Geocoder();
 
   function setUpMap (geoObject) {
@@ -30,35 +31,42 @@ function initialize() {
       center: latlng
     }
     map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
+
     yourMarker = new google.maps.Marker({
       map: map,
       position: latlng
     });
-    // console.log(`this is your marker ${yourMarker}`)
-  }
 
-  codeAddress(document.getElementById('googleMap').dataset.userLocation, setUpMap)
-  mealPostingMarkers()
+  }
+  const setMapAndYourMarker = new Promise((resolve, reject) => {
+    codeAddress(document.getElementById('googleMap').dataset.userLocation, setUpMap)
+    resolve(mealPostingMarkers)
+  })
+  setMapAndYourMarker.then((callback) => {
+    callback()
+  })
+
+
 }
 
 
-function orderMarkerByProximity() {
-  // console.log(yourMarker)
+function orderMarkerByProximity(geoObject) {
+
   labelIndex = 0
-  if (yourMarker) {
+
     postingMarkers.sort(function(a, b){
+      console.log(`inside the sort function${yourMarker}`)
       let aLat = a.position.lat()
       let aLng = a.position.lng()
       let bLat = b.position.lat()
       let bLng = b.position.lng()
-      let mLat = yourMarker.position.lat()
-      let mLng = yourMarker.position.lng()
+      let mLat = geoObject[0].geometry.location.lat()
+      let mLng = geoObject[0].geometry.location.lng()
 
       let aDistance = Math.sqrt(Math.pow((aLat - mLat), 2) + Math.pow((aLng - mLng), 2))
       let bDistance = Math.sqrt(Math.pow((bLat - mLat), 2) + Math.pow((bLng - mLng), 2))
       return (aDistance - bDistance)
     })
-  }
   for (let marker of postingMarkers) {
 
     marker.label = labels[labelIndex++ % labels.length]
@@ -69,67 +77,35 @@ function orderMarkerByProximity() {
 function mealPostingMarkers() {
 
   function mealMarkerCallback (geoObject) {
-    function checkLocation(postings) {
-      if (geoCount === 0) { return false }
-      return (geoObject[0].formatted_address === postings.address)
-    }
-// redo this better, still throwing google maps api error
-    if(!postingMarkers.find(checkLocation)){
-      let marker = new google.maps.Marker({
-        map: map,
-        position: geoObject[0].geometry.location,
-        label: labels[labelIndex++ % labels.length],
-        address: geoObject[0].formatted_address
-      })
-      postingMarkers.push(marker)
 
-      orderMarkerByProximity()
-    }
+    let marker = new google.maps.Marker({
+      map: map,
+      position: geoObject[0].geometry.location,
+      label: labels[labelIndex++ % labels.length],
+      address: geoObject[0].formatted_address
+    })
+    postingMarkers.push(marker)
+    codeAddress(document.getElementById('googleMap').dataset.userLocation, orderMarkerByProximity)
   }
   for (let marker of postingMarkers){
     marker.setMap(null)
   }
   postingMarkers.length = 0
 
-  let geoCount = 0
   let userAddresses = []
   for (let element of document.getElementsByClassName("meal-div")) {
-
     let address = element.dataset.address
-    geoCount++
-    if (geoCount === 0) {
-      setTimeout(function() {codeAddress(address, mealMarkerCallback)}, 1300);
-    } else {
-      codeAddress(address, mealMarkerCallback)
+    if (!userAddresses.includes(address)){
+      userAddresses.push(address)
+      if (userAddresses.length === 10) {
+        setTimeout(function() {codeAddress(address, mealMarkerCallback)}, 1300);
+      } else {
+        codeAddress(address, mealMarkerCallback)
+
+      }
     }
   }
+
 }
 
-
-function moveYourLocation() {
-
-  // setMapOnAll(null)
-
-  let address = document.getElementById('address').value;
-  codeAddress(address, function(geoObject){
-    map.setCenter(geoObject[0].geometry.location)
-  })
-  codeAddress(address, function (geoObject) {
-    if (yourMarker){
-     yourMarker.setMap(null)
-    }
-    yourMarker = new google.maps.Marker({
-      map: map,
-      title: "Your Location!",
-      position: geoObject[0].geometry.location
-    });
-    // console.log(postingMarkers)
-    mealPostingMarkers()
-  })
-    // postingMarkers.length = 0
-
-
-  // orderMarkerByProximity()
-  // console.log("reaching here")
-}
 
