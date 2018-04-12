2ebc19a4965d6e0488aeeef915007a5a3cc1db65
Greg Davis <gdavis111@live.ca>
2018-04-12 00:33:49 +0000

    Restricted users from reviewing themselves or reserving their own meals. Now subtracts servings when a reservation is accepted

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -71,6 +71,13 @@ class ReservationsController < ApplicationController
     @reservation = Reservation.find(params[:id])
     @reservation.status = "Accepted"
     @reservation.save
+
+    # SUBTRACTS NUMBER OF SERVINGS ON RESERVATION FROM THE MEAL POSTING'S SERVINGS
+    @meal_posting = MealPosting.find(params[:id])
+    puts "-------------------- #{@meal_posting.inspect} -----------------------"
+    @meal_posting.servings = @meal_posting.servings - @reservation.number_of_people
+    puts "-------------------- #{@meal_posting.inspect} -----------------------"
+    @meal_posting.save
     redirect_to reservations_url
   end
 
@@ -84,7 +91,11 @@ class ReservationsController < ApplicationController
 
   # ADDED FOR SHOWING HOSTS RESERVATIONS FOR THEIR MEALS
   def outgoingreservations
-    @user = User.find(current_user.id)
+    if current_user.user_status === "Host"
+      @user = User.find(current_user.id)
+    else
+      redirect_to root_path
+    end
   end
 
   private
