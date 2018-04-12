95907665b9bd5017a99a1ff95d273dcda08e449a
Piotr <bartnic1@gmail.com>
2018-04-12 22:47:19 +0000

    added validation to reservation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,6 @@
 class Reservation < ApplicationRecord
   validates :number_of_people, numericality: {greater_than: 0}
-
+  validates :time, presence: true
 
   belongs_to :user
   belongs_to :meal_posting
