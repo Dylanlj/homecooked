85eeb9dbff00169991fa943bef6259ef209a9a83
Piotr <bartnic1@gmail.com>
2018-04-18 23:05:44 +0000

    latest changes to analytics

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,5 +1,20 @@
 class AnalyticsController < ApplicationController
   def index
-    # @reservations
+    # @categories = Category.all
+    @mealpostings = MealPosting.all
+    # counter = []
+    categories = ["1", "2", "3"]
+    # categories.each do |category_id|
+    #   counter.push(@mealpostings.count(:conditions => "category_id = #{category_id}"))
+    # end
+    # puts "==========HELLO=========="
+    categories.each do |category_id|
+      puts category_id
+    end
+
+
+    @selectedmeals = MealPosting.where("category_id = 1").size
+    puts "=====HELLO====="
+    puts @selectedmeals
   end
 end
