d846095761db8d17836927ab36d27c43cad344f9
Piotr <bartnic1@gmail.com>
2018-04-19 15:15:45 +0000

    created sample pie graph instead

--- app/assets/javascripts/analytics.js
+++ app/assets/javascripts/analytics.js
@@ -2,7 +2,7 @@
 // All this logic will automatically be available in application.js.
 // You can use CoffeeScript in this file: http://coffeescript.org/
 
-$(document).ready(function(){
-  $(".pie-chart").css("background", "${conic-gradient($red 4%, $gray 0 8%, $blue 0 17%, $yellow 0 48%, $purple 0 74%, $orange 0)}");
-  $(".choice-1").text("#{@selectedmeals}");
-})
+// $(document).ready(function(){
+  // $(".pie-chart").css("background", "${conic-gradient($red 4%, $gray 0 8%, $blue 0 17%, $yellow 0 48%, $purple 0 74%, $orange 0)}");
+  // $(".choice-1").text("#{@selectedmeals}");
+// })
--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,19 +1,27 @@
 class AnalyticsController < ApplicationController
   def index
     # @categories = Category.all
-    @mealpostings = MealPosting.all
-    @counter = []
-    @categories = ["1", "2", "3", "4", "5"]
-    # categories.each do |category_id|
-    #   counter.push(@mealpostings.count(:conditions => "category_id = #{category_id}"))
-    # end
-    # puts "==========HELLO=========="
-    @categories.each do |category_id|
-      size = MealPosting.where("category_id = #{category_id}").size
-      counter.push(size)
-    end
+    # @mealpostings = MealPosting.all
+    # @categories = Category.all
 
-    puts "=====HELLO=====#{@counter}"
-    @counter
+    # @percents_init = []
+    # @percents = []
+    # @category_ids = ["1", "2", "3", "4", "5", "6" ,"7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17"]
+    # total = 0
+
+    # @category_ids.each do |category_id|
+    #   size = MealPosting.where("category_id = #{category_id}").size
+    #   total += size
+    #   @percents_init.push(size)
+    # end
+
+    # @percents_init.each do |init|
+    #   percentage = init*100/total
+    #   @percents.push(percentage)
+    # end
+
+
+    # puts "=====HELLO=====#{@percents}"
+    # @counter
   end
 end
